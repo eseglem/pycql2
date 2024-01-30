7944878c74b2fcdea1f12fbc0240379de25959a2
Erik Seglem <erik.seglem@gmail.com>
2024-01-30 13:30:23 -0500

    Update tests to use pathlib.

--- tests/test_pycql2.py
+++ tests/test_pycql2.py
@@ -1,5 +1,4 @@
-import json
-import os
+from pathlib import Path
 
 import pytest
 from lark import UnexpectedCharacters
@@ -8,85 +7,71 @@ from pydantic import ValidationError
 from pycql2.cql2_pydantic import BooleanExpression
 from pycql2.cql2_transformer import parser, transformer
 
-JSON_DIR = "tests/data/json"
-TEXT_DIR = "tests/data/text"
-json_files = os.listdir(JSON_DIR)
-text_files = os.listdir(TEXT_DIR)
+JSON_DIR = Path("tests/data/json")
+TEXT_DIR = Path("tests/data/text")
+json_files = list(JSON_DIR.glob("*.json"))
+text_files = list(TEXT_DIR.glob("*.txt"))
 
-BAD_JSON_DIR = "tests/bad_data/json"
-BAD_TEXT_DIR = "tests/bad_data/text"
-bad_json_files = os.listdir(BAD_JSON_DIR)
-bad_text_files = os.listdir(BAD_TEXT_DIR)
+BAD_JSON_DIR = Path("tests/bad_data/json")
+BAD_TEXT_DIR = Path("tests/bad_data/text")
+bad_json_files = list(BAD_JSON_DIR.glob("*.json"))
+bad_text_files = list(BAD_TEXT_DIR.glob("*.txt"))
 
 
 @pytest.mark.parametrize("json_file", json_files)
-def test_parse_json(json_file: str) -> None:
-    # Simply test that the pydantic model can parse the json
-    with open(os.path.join(JSON_DIR, json_file)) as inf:
-        json_data = json.load(inf)
-    _ = BooleanExpression.model_validate(json_data)
+def test_parse_json(json_file: Path) -> None:
+    """Test that the pydantic model can parse the json."""
+
+    _ = BooleanExpression.model_validate_json(json_file.read_text())
 
 
 @pytest.mark.parametrize("text_file", text_files)
-def test_parse_text(text_file: str) -> None:
-    # Simply test that the parser can parse the text
-    with open(os.path.join(TEXT_DIR, text_file)) as inf:
-        text_data = inf.read()
-    _ = parser.parse(text_data)
+def test_parse_text(text_file: Path) -> None:
+    """Test that the parser can parse the text."""
+
+    _ = parser.parse(text_file.read_text())
 
 
 @pytest.mark.parametrize("json_file", bad_json_files)
-def test_parse_bad_json(json_file: str) -> None:
-    # Simply test that the pydantic model can parse the json
-    with open(os.path.join(BAD_JSON_DIR, json_file)) as inf:
-        json_data = json.load(inf)
+def test_parse_bad_json(json_file: Path) -> None:
+    """Test that the pydantic model does not parse the bad json."""
+
     with pytest.raises(ValidationError):
-        _ = BooleanExpression.model_validate(json_data)
+        _ = BooleanExpression.model_validate_json(json_file.read_text())
 
 
 @pytest.mark.parametrize("text_file", bad_text_files)
-def test_parse_bad_text(text_file: str) -> None:
-    # Simply test that the parser can parse the text
-    with open(os.path.join(BAD_TEXT_DIR, text_file)) as inf:
-        text_data = inf.read()
+def test_parse_bad_text(text_file: Path) -> None:
+    """Test that the parser does not parse the bad text."""
+
     with pytest.raises(UnexpectedCharacters):
-        _ = parser.parse(text_data)
+        _ = parser.parse(text_file.read_text())
 
 
 @pytest.mark.parametrize("json_file", json_files)
-def test_json_to_text(json_file: str) -> None:
-    # Test that the pydantic model outputs the correct text representation
+def test_json_to_text(json_file: Path) -> None:
+    """Test that the pydantic model outputs the correct text representation."""
 
     # Load the json data
-    with open(os.path.join(JSON_DIR, json_file)) as inf:
-        json_data = json.load(inf)
-    model = BooleanExpression.model_validate(json_data)
+    model = BooleanExpression.model_validate_json(json_file.read_text())
     # Load the expected text data
-    text_file = json_file.replace(".json", ".txt")
-    with open(os.path.join(TEXT_DIR, text_file)) as inf:
-        expected = inf.read().strip()
+    text_file = TEXT_DIR / (json_file.stem + ".txt")
+    expected = text_file.read_text().strip()
     # Compare the two
     assert str(model) == expected
 
 
 @pytest.mark.parametrize("text_file", text_files)
-def test_text_to_json(text_file: str) -> None:
-    # Test that the parser outputs the correct json representation
+def test_text_to_json(text_file: Path) -> None:
+    """Test that the parser outputs the correct json representation."""
 
     # Load the text data
-    with open(os.path.join(TEXT_DIR, text_file)) as inf:
-        text_data = inf.read()
-    model = parser.parse(text_data)
-    output = transformer.transform(model)
-
+    tree = parser.parse(text_file.read_text())
+    output = transformer.transform(tree)
+    # Some text files are equivalent to the same json file. These contain
+    # "-alt" in the file names. We split on the "-" to get the base name.
+    json_file = JSON_DIR / (text_file.stem.split("-")[0] + ".json")
     # Load the expected json data
-    json_file = text_file.replace(".txt", ".json")
-    # Some text files have an "alt" suffix, which means that they are
-    # equivalent to the same json file.  In this case, we want to load
-    # the json file without the "alt" suffix.
-    if "alt" in json_file:
-        json_file = json_file.split("-")[0] + ".json"
-    with open(os.path.join(JSON_DIR, json_file)) as inf:
-        json_data = json.load(inf)
-    expected = BooleanExpression.model_validate(json_data)
+    expected = BooleanExpression.model_validate_json(json_file.read_text())
+    # Compare the two
     assert output == expected
